44c2467153fc6b5a287c8e9df37c24be0cd702bd
Naiim Hassouna <naiimhassouna0@gmail.com>
2025-02-12 16:46:47 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { ExpoRoot } from "expo-router";
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+import { UserProvider } from './context/UserContext';
+import { ThemeProvider } from './context/ThemeContext';
+
+export default function App() {
+  const ctx = require.context("./app/_tabs");
+
+  return (
+    <UserProvider>
+      <ThemeProvider>
+        <ExpoRoot context={ctx} />
+      </ThemeProvider>
+    </UserProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => App);
